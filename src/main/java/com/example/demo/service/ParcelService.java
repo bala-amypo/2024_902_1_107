64a8dce523796fdb43a234dfa98b0c3bc28fe9dc
bala-amypo <bala@amypo.in>
2025-12-21 15:43:14 +0000

    User commit

--- src/main/java/com/example/demo/service/ParcelService.java
+++ src/main/java/com/example/demo/service/ParcelService.java
@@ -1,7 +1,16 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Parcel;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ParcelService {
+
+    public Parcel addParcel(Parcel parcel) {
+        return parcel;
+    }
+
+    public Parcel getByTrackingNumber(String trackingNumber) {
+        return null;
+    }
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,7 +1,16 @@
 package com.example.demo.service;
 
+import com.example.demo.model.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    public User register(User user) {
+        return user;
+    }
+
+    public User findByEmail(String email) {
+        return null;
+    }
 }
