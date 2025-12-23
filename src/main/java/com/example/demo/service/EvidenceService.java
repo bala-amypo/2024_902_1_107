c5a738fef8eb56647e71fa82c38c2fe361754bd0
bala-amypo <bala@amypo.in>
2025-12-23 10:31:41 +0000

    User commit

--- src/main/java/com/example/demo/service/EvidenceService.java
+++ src/main/java/com/example/demo/service/EvidenceService.java
@@ -1,18 +1,9 @@
 package com.example.demo.service;
 
-import com.example.demo.model.Evidence;
-import org.springframework.stereotype.Service;
-
+import com.example.demo.entity.Evidence;
 import java.util.List;
 
-@Service
-public class EvidenceService {
-
-    public Evidence uploadEvidence(Long claimId, Evidence evidence) {
-        return evidence;
-    }
-
-    public List<Evidence> getEvidenceForClaim(Long claimId) {
-        return List.of();
-    }
+public interface EvidenceService {
+    Evidence uploadEvidence(Long claimId, Evidence evidence);
+    List<Evidence> getEvidenceForClaim(Long claimId);
 }
