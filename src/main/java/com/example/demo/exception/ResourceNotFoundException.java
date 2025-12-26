8b2cb787bdd267b00318b7c97e6236ad4f834ade
bala-amypo <bala@amypo.in>
2025-12-26 17:38:38 +0000

    User commit

--- src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -1,8 +1,10 @@
-package com.example.demo.exception;
+package com.example.demo.repository;
 
-public class BadRequestException extends RuntimeException {
+import com.example.demo.model.Evidence;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-    public BadRequestException(String message) {
-        super(message);
-    }
+import java.util.List;
+
+public interface EvidenceRepository extends JpaRepository<Evidence, Long> {
+    List<Evidence> findByClaim_Id(Long claimId);
 }
