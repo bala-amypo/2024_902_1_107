d772e277ce29e1bef2d14fb1d573e023b91a2940
bala-amypo <bala@amypo.in>
2025-12-27 13:09:16 +0000

    User commit

--- src/main/java/com/example/demo/service/DamageClaimService.java
+++ src/main/java/com/example/demo/service/DamageClaimService.java
@@ -3,15 +3,9 @@ package com.example.demo.service;
 import com.example.demo.model.DamageClaim;
 import java.util.List;
 
-package com.example.demo.service;
-
-import com.example.demo.model.DamageClaim;
-import java.util.List;
-
 public interface DamageClaimService {
 
     DamageClaim createClaim(Long parcelId, DamageClaim claim);
 
     List<DamageClaim> getAllClaims();
 }
-
